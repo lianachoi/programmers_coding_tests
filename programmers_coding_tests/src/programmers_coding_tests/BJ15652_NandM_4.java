d58fca6cbaedaa818062f56ceb655cf860b44426
DESKTOP-D878T6V\liana <2ndsunmail@gmail.com>
2021-10-19 01:57:01 +0900

    중복조합 nHm

---
+++ programmers_coding_tests/src/programmers_coding_tests/BJ15652_NandM_4.java
@@ -0,0 +1,58 @@
+package programmers_coding_tests;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+/*
+ * 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+ * 1부터 N까지 자연수 중에서 M개를 고른 수열
+ * 같은 수를 여러 번 골라도 된다.
+ * 고른 수열은 비내림차순이어야 한다.
+ * 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+ * 
+ * 중복조합 nHm
+*/
+public class BJ15652_NandM_4 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N;
+	static int M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder(); 
+	
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		
+		backTracking(1, 0);
+		
+		bw.write(sb.toString());
+		
+		br.close();
+		bw.close();
+	}
+
+	private static void backTracking(int at, int depth) {
+		if (depth == M) {
+			for (int i = 0; i < arr.length; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = at; i <= N; i++) {
+			arr[depth] = i;
+			backTracking(i, depth+1);
+		}
+	}
+
+}
