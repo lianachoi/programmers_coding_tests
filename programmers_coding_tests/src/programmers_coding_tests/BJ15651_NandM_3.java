a7693af4e89e0f58c81148fb148ca0940946a43d
DESKTOP-D878T6V\liana <2ndsunmail@gmail.com>
2021-10-19 01:56:45 +0900

    중복순열 nΠm

---
+++ programmers_coding_tests/src/programmers_coding_tests/BJ15651_NandM_3.java
@@ -0,0 +1,54 @@
+package programmers_coding_tests;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+/*자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+ * 1부터 N까지 자연수 중에서 M개를 고른 수열
+ * 같은 수를 여러 번 골라도 된다.
+ * 중복순열 nΠm
+*/
+public class BJ15651_NandM_3 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N;
+	static int M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder(); 
+	
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		
+		backTracking(0);
+		
+		bw.write(sb.toString());
+		
+		br.close();
+		bw.close();
+	}
+
+	private static void backTracking(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < arr.length; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			arr[depth] = i;
+			backTracking(depth+1);
+		}
+	}
+
+}
